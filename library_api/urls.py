51490190ad304c684b1e063f28dfa137fe005ddf
Anastasiia Temnova <temnovastudy@gmail.com>
2020-07-05 17:08:50 +0300

    added search api

--- library_api/urls.py
+++ library_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from library_api.views import BookViewSet, AuthorViewSet, FollowerViewSet, LanguageViewSet
+from library_api.views import BookViewSet, AuthorViewSet, FollowerViewSet, LanguageViewSet, BookSearchList
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -28,5 +28,6 @@ router.register(r'follower', FollowerViewSet)
 urlpatterns = router.urls
 
 urlpatterns += [
+    url(r'^search/', BookSearchList.as_view(), name='search'),
     url(r'^schema/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- library_api/views.py
+++ library_api/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
+from django.db import models
+from django.utils.decorators import method_decorator
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, generics, filters
+from rest_framework.exceptions import ValidationError
 
-from library_api import serializers
 from library_api import models as library_models
+from library_api import serializers
 
 
 class LanguageViewSet(viewsets.ModelViewSet):
@@ -22,3 +27,27 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class FollowerViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.Follower
     queryset = library_models.Follower.objects.all()
+
+
+class BookSearchFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        author_number = request.query_params.get('author_number', None)
+        if author_number is None:
+            raise ValidationError(detail='parameter "author_number" is required')
+        return queryset.annotate(
+            author_number=models.Count('authors')
+        ).filter(author_number=author_number)
+
+
+class BookSearchList(generics.ListAPIView):
+    queryset = library_models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
+    filter_backends = [BookSearchFilterBackend]
+    filterset_fields = ['author_number']
+
+    @method_decorator(swagger_auto_schema(manual_parameters=[
+        openapi.Parameter(name='author_number', in_=openapi.IN_QUERY, type='integer', description='Количество авторов',
+                          required=True)],
+    ), name='search')
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
